1c2447d12c87a69ee19a6897ec127bbadbf1cb9d
firstkraken <47188465+firstkraken@users.noreply.github.com>
2019-02-01 21:26:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ Sellcost = 50
 multiplier = 1
 power = 0
 mantissa = 0
+carrers = 0
+var lastUpdate = Date.now()
 function format(amount) {
   power = Math.floor(Math.log10(amount))
   mantissa = amount / Math.pow(10, power)
@@ -30,11 +32,13 @@ function sellIcecream() {
   document.getElementById("currency").innerHTML = "You have $" + format(money)
 }
 function decreaseBuy() {
-  if (icecream >= Buycost.toFixed(0)) {
-    icecream -= Buycost.toFixed(0)
-    Buycost *= 1.1
-    icecreamBuy -= icecreamBuy*0.01
-    document.getElementById("Buycost").innerHTML = "cost " + format(Buycost) + " Icecream"
+  if (icecreamBuy > 0.01) {
+    if (icecream >= Buycost.toFixed(0)) {
+      icecream -= Buycost.toFixed(0)
+      Buycost *= 10
+      icecreamBuy -= icecreamBuy*0.01
+      document.getElementById("Buycost").innerHTML = "cost: " + format(Buycost) + " Icecream"
+    }
   }
   document.getElementById("icecream").innerHTML = "You have " + format(icecream) + " icecream!"
   document.getElementById("currency").innerHTML = "You have $" + format(money)
@@ -42,7 +46,7 @@ function decreaseBuy() {
 function increaseSell() {
   if (money >= Sellcost.toFixed(0)) {
     money -= Sellcost.toFixed(0)
-    Sellcost *= 1.2
+    Sellcost *= 2
     icecreamSell += 0.1
     document.getElementById("Sellcost").innerHTML = "cost: $" + format(Sellcost)
   }
@@ -59,3 +63,39 @@ function minusMultiplier() {
   }
   document.getElementById("Multiplier").innerHTML = "Multiplier" + format(multiplier) +"x"
 }
+function update(){
+  document.getElementById("icecream").innerHTML = "You have " + format(icecream) + " icecream!"
+  document.getElementById("currency").innerHTML = "You have $" + format(money)
+  if (icecream >= 10**(10*(carrers+1))){
+    if (money >= 10**(10*(carrers+1))){
+      document.getElementById("carrer").classList.add('open')
+    }
+  }
+}
+function newCarrer() {
+  if (icecream >= 10**(10*(carrers+1))){
+    if (money >= 10**(10*(carrers+1))){
+      icecream = 0
+      money= 10
+      Buycost = 10
+      icecreamBuy = .5
+      icecreamSell = 1
+      Sellcost = 50
+      multiplier = 1
+      power = 0
+      mantissa = 0
+      carrers += 1
+      icecreamSell += carrers
+      document.getElementById("carrer").classList.remove('open')
+      document.getElementById("Buycost").innerHTML = "cost: " + format(Buycost) + " Icecream"
+      document.getElementById("Sellcost").innerHTML = "cost: $" + format(Sellcost)
+      document.getElementById("Multiplier").innerHTML = "Multiplier" + format(multiplier) +"x"
+    }
+  }
+}
+function timer() {
+  var diff = (Date.now() - lastUpdate) / 1000
+  update()
+  lastUpdate = Date.now()
+}
+setInterval(timer, 50)
